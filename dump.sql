7d48a54264a75d065b6d9249b7229d67c2a23c61
TranHuy2k2 <giahuytran2002@gmail.com>
2023-10-03 03:26:51 +0700

    chore: export db

--- dump.sql
+++ dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Oct 02, 2023 at 02:15 PM
+-- Generation Time: Oct 02, 2023 at 08:25 PM
 -- Server version: 8.1.0
 -- PHP Version: 8.2.10
 
@@ -32,7 +32,8 @@ CREATE TABLE `blogs` (
   `title` text NOT NULL,
   `content` text NOT NULL,
   `image` text NOT NULL,
-  `author_id` int NOT NULL
+  `author_id` int NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -47,6 +48,16 @@ CREATE TABLE `users` (
   `name` text NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`) VALUES
+(3, 'giahuytran2002@gmail.com', 'Tran Gia Huy', '$2y$10$lKE7c4yZlfWN9oVUuY0eP.J.vI84W8fiECI0vd0Ie0i.5ehus4oBC'),
+(4, 'giahuytran2002@gmail.com', 'Tran Gia Huy', '$2y$10$7KjnObVpMFkV0uxR4Rl0MuJ9y9WTyvbqPvpvBp9AFCH1hWuVO0/ti'),
+(5, 'giahuytran2002@gmail.com', 'Tran Gia Huy', '$2y$10$UXMf9rfB.rgShwQ9EEIXp.fjdRZSXZFhN4DeDcqszjd8I6OffTpUK'),
+(6, 'giahuytran2002@gmail.com', 'Tran Gia Huy', '$2y$10$5wb5tG8bo/kcjAlr/2OpWOA/kp2Vl.AmzY2yIOlFLe7eVfltkSaJm');
 
 --
 -- Indexes for dumped tables
@@ -73,13 +84,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
